62c6e2a668ffc2cd378305ee90b263597adadbfa
Brownie Tran <tknguyen2111@gmail.com>
2025-05-08 08:48:40 -0700

    Finish profile page frontend (without notification popup)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
-import './styles/App.css'
+import './styles/profile.css'
 import Landing from './pages/landing';
+import Profile from './pages/profile';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<Profile />} />
     </Routes>
   );
 }
--- src/sections/options.jsx
+++ src/sections/options.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 
 export default function Options() {
+
+    const strokeWidth = 0.65;
+
     const options = [
         {
             id: 1,
-            icon: "",
+            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke="currentColor" className="icon">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                  </svg>,
             title: "Profile details"
         },
         {
             id: 2,
-            icon: "",
+            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke="currentColor" className="icon">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
+                  </svg>,
             title: "Settings"
         },
         {
             id: 3,
-            icon: "",
+            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke="currentColor" className="icon">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                  </svg>,
             title: "Log out"
         }
     ];
@@ -24,8 +33,9 @@ export default function Options() {
         <div className="profile-grid">
             {options.map(profile => (
                 <div key={profile.id} className="profile-card">
-                    <div className="profile-icon">{profile.icon}</div>
-                    <h4>{profile.title}</h4>
+                    {profile.icon}
+                    <div className="profile-icon"></div>
+                    <h3>{profile.title}</h3>
                 </div>
             ))}
         </div>
