32de460c35465c12b00717d4c1ae3bf9ff7bbc9b
luis-Saberon <luis.b.r.saberon@gmail.com>
2025-05-07 18:07:46 -0700

    Added comments for the usage of the database connection

--- server/databaseConnection.cjs
+++ server/databaseConnection.cjs
@@ -4,6 +4,13 @@
  * @see https://www.youtube.com/watch?v=CE1H4t8t2yY
  */
 const {MongoClient} = require('mongodb');
+
+/**
+ * You'll have to make your own config.env file in this folder, looking like this:
+ * ATLAS_URI=<connection_string>
+ * 
+ * with connection_string looking something like this: mongodb+srv://<username>:<password>@playpal.bv02d2k.mongodb.net/
+ */
 require('dotenv').config({path: "./config.env"});
 
 const client = new MongoClient(process.env.ATLAS_URI, {});
