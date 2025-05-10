ae4fc61620f4718311f6752c8a926de3625a930b
RAYMOND-ZENBOOK\Raymond <raymondyang59@gmail.com>
2025-05-09 17:21:31 -0700

    Fixed navbar merge error

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 import ClickableIcons from "../ui/clickableIcons";
 
-export default function Navbar({iconLeft, iconRight, header = "header"}) {
+
 export default function Navbar({iconLeft, iconRight, header = "header"}) {
   return (
-    <nav className="flex items-center justify-between px-4 py-3 ">
-      <div className="w-10">
-        <ClickableIcons icon={iconLeft} />
-      </div>
-      <div className="flex-1 text-left pl-4">
-        <h4>{header}</h4>
-      </div>
-      <div className="w-10 text-right">
-        <ClickableIcons icon={iconRight} />
-      </div>
     <nav className="flex items-center justify-between px-4 py-3 ">
       <div className="w-10">
         <ClickableIcons icon={iconLeft} />
