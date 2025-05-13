929ada77efcfa49ff9dbfc1d9392051b799397e9
RAYMOND-ZENBOOK\Raymond <raymondyang59@gmail.com>
2025-05-13 10:57:43 -0700

    completed index page frontend

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 import ClickableIcons from "../ui/clickableIcons";
 
 
-export default function Navbar({iconLeft, iconRight, header = "header"}) {
+export default function Navbar({iconLeft, iconRight, iconRight2, header = "header"}) {
   return (
     <nav className="flex items-center justify-between px-4 py-3 ">
       <div className="w-10">
         <ClickableIcons icon={iconLeft} />
       </div>
       <div className="flex-1 text-left pl-4">
-        <h4>{header}</h4>
+        <h2>{header}</h2>
       </div>
       <div className="w-10 text-right">
         <ClickableIcons icon={iconRight} />
+        <ClickableIcons icon={iconRight2} />
       </div>
     </nav>
   );
