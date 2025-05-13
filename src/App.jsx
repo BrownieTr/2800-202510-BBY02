f62f10eb6fdd493b6564073caf24c82da05ebaee
Brownie Tran <tknguyen2111@gmail.com>
2025-05-13 14:59:27 -0700

    Add css file for profile page to dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import './styles/App.css';
+import './styles/profile.css';
 import Landing from './pages/landing';
 import Profile from './pages/profile';
 import Location from './services/locationService';  
