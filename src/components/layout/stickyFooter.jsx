2ffd5c0b1ad255ae6c991c86c2b6f2b3f7afce86
RAYMOND-ZENBOOK\Raymond <raymondyang59@gmail.com>
2025-05-14 16:29:46 -0700

    added a sticky footer to events page

---
+++ src/components/layout/stickyFooter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function StickyFooter({ iconLeft, iconRight, iconMiddle }) {
+  return (
+    <footer className="sticky bottom-0 z-50 bg-white w-full">
+      <div className="container mx-auto flex justify-around items-center">
+        <div className="p-2">{iconLeft}</div>
+        <div className="p-2">{iconMiddle}</div>
+        <div className="p-2">{iconRight}</div>
+      </div>
+    </footer>
+  );
+}
--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../components/layout/navbar";
 import EventCard from "../components/ui/eventCard";
+import Footer from "../components/layout/stickyFooter";
+import ClickableIcons from "../components/ui/clickableIcons";
 
 export default function events() {
   return (
@@ -32,11 +34,66 @@ export default function events() {
         }
         iconRight2To={"/messages"}
       />
-      <div className="mt-20 flex flex-col gap-4">
+      <div className="mt-5 mb-5 flex flex-col gap-4 overflow-y-auto max-h-[calc(100vh-160px)] p-4">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
+        <EventCard />
 
-      <EventCard/>
-      <EventCard/>
       </div>
+      <Footer
+        iconLeft={
+          <ClickableIcons
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#000000"
+              >
+                <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+              </svg>
+            }
+          />
+        }
+        iconMiddle={
+          <ClickableIcons
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#000000"
+              >
+                <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+              </svg>
+            }
+            to={"/createEvent"}
+          />
+        }
+        iconRight={
+          <ClickableIcons
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#000000"
+              >
+                <path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Zm80-122 200-86 200 86v-518H280v518Zm0-518h400-400Z" />
+              </svg>
+            }
+          />
+        }
+      />
     </>
   );
 }
