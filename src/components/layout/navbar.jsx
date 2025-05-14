5ecbcfa6fb80918f33485bb67bd0b3976222177a
RAYMOND-ZENBOOK\Raymond <raymondyang59@gmail.com>
2025-05-14 00:17:57 -0700

    Linked pages and fixed some visual bugs

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import ClickableIcons from "../ui/clickableIcons";
 
-
-export default function Navbar({iconLeft, iconRight, iconRight2, header = "header", iconLeftTo, iconRightTo, iconRight2To}) {
+export default function Navbar({
+  iconLeft,
+  iconRight,
+  iconRight2,
+  header = "header",
+  iconLeftTo,
+  iconRightTo,
+  iconRight2To,
+}) {
   return (
-    <nav className="flex items-center justify-between px-4 py-3 ">
-      <div className="w-10">
+    <nav className="flex items-center justify-between ">
+      <div>
         <ClickableIcons icon={iconLeft} to={iconLeftTo} />
       </div>
-      <div className="flex-1 text-left pl-4">
+      <div className="flex-1 text-left">
         <h2>{header}</h2>
       </div>
       <div className="w-10 text-right">
-        <ClickableIcons icon={iconRight} to={iconRightTo}/>
-        <ClickableIcons icon={iconRight2} to={iconRight2To}/>
+        <ClickableIcons icon={iconRight} to={iconRightTo} />
+      </div>
+      <div className="w-10 text-right">
+        <ClickableIcons icon={iconRight2} to={iconRight2To} />
       </div>
     </nav>
   );
 }
-
