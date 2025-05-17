b52c09d7ba89185a5602cb49d154079725032ad4
RAYMOND-ZENBOOK\Raymond <raymondyang59@gmail.com>
2025-05-16 17:56:53 -0700

    added more protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-
 import { Routes, Route } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import './styles/App.css';
 import Landing from './pages/landing';
 import Profile from './pages/profile';
-// import Location from './services/locationService';  
+import Location from './services/locationService';
 import Event from './pages/events';
 import Chat from './pages/chat';
 import ProtectedRoute from './services/protectedRoutes';
-import Messages from "./pages/messages";
-import Index from "./pages/index";
+import Messages from './pages/messages';
+import CreateEvent from './pages/createEvent';
+import FavSport from './pages/favSport';
+import FindingMatch from './pages/findingMatch';
+import SportsBetting from './pages/sportsBetting';
+import Index from './pages/index';
 
 function App() {
   const { isLoading } = useAuth0();
@@ -24,36 +27,16 @@ function App() {
       <Route path="/" element={<Landing />} />
       
       {/* Protected routes - require authentication */}
-      <Route path="/profile" element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      } />
-      <Route path="/location" element={
-        <ProtectedRoute>
-          <Location />
-        </ProtectedRoute>
-      } />
-      <Route path="/events" element={
-        <ProtectedRoute>
-          <Event />
-        </ProtectedRoute>
-      } />
-      <Route path="/chat" element={
-        <ProtectedRoute>
-          <Chat />
-        </ProtectedRoute>
-      } />
-      <Route path="/messages" element={
-        <ProtectedRoute>
-          <Messages />
-        </ProtectedRoute>
-      } />
-      <Route path="/home" element={
-        <ProtectedRoute>
-          <Index />
-        </ProtectedRoute>
-      } />
+      <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+      <Route path="/location" element={<ProtectedRoute><Location /></ProtectedRoute>} />
+      <Route path="/events" element={<ProtectedRoute><Event /></ProtectedRoute>} />
+      <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
+      <Route path="/messages" element={<ProtectedRoute><Messages /></ProtectedRoute>} />
+      <Route path="/home" element={<ProtectedRoute><Index /></ProtectedRoute>} />
+      <Route path="/createEvent" element={<ProtectedRoute><CreateEvent /></ProtectedRoute>} />
+      <Route path="/favSport" element={<ProtectedRoute><FavSport /></ProtectedRoute>} />
+      <Route path="/findingMatch" element={<ProtectedRoute><FindingMatch /></ProtectedRoute>} />
+      <Route path="/sportsBetting" element={<ProtectedRoute><SportsBetting /></ProtectedRoute>} />
     </Routes>
   );
 }
