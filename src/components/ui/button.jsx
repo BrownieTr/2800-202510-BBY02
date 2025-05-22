f817e6c0f4b6e8939ea96af9c3804c5de20740dd
Brownie Tran <tknguyen2111@gmail.com>
2025-05-21 17:03:59 -0700

    Add logout button, finish profile CSS, delete Settings in the mean time

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 export default function Button({ 
   children, 
-  className = '',
+  className,
   type = 'button',
   onClick,
   ...props 
 }) {
   return (
     <button  
-      className={`px-3 py-1 rounded-xl bg-blue-600 text-white font-semibold 
-      hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 
-      shadow hover:shadow-lg transition duration-300 ease-in-out ${className}`}
+      className={`px-3 py-1 rounded-xl text-white font-semibold 
+      hover:bg-black-700 focus:outline-none focus:ring-2 focus:ring-blue-400 
+      shadow hover:shadow-lg transition-transform hover:scale-105 duration-300 ease-in-out ${className}`}
       type={type}
       onClick={onClick}
       {...props}
