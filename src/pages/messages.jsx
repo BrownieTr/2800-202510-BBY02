85819dc4c6cd8f1bfc3b14794956419dba20e623
Brownie Tran <tknguyen2111@gmail.com>
2025-05-21 22:02:06 -0700

    Fix message page's navbar

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -4,6 +4,7 @@ import MessageCard from "../components/ui/messageCard";
 import BackButton from "../components/ui/backButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Messages() {
   // Destructure Auth0 hooks for authentication and user data
@@ -14,6 +15,7 @@ export default function Messages() {
   const [conversations, setConversations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   // Fetch conversations when the user is authenticated
   useEffect(() => {
@@ -81,7 +83,20 @@ export default function Messages() {
       <nav className="sticky top-0 z-50 bg-white">
         <Navbar
           iconLeft={<BackButton />}
-          iconRight2={
+          header="Messages"
+          iconRight={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
+            </svg>
+        }
+        iconRightTo={() => navigate('/profile')}
+        iconRight2={
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
@@ -92,9 +107,8 @@ export default function Messages() {
             >
               <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
             </svg>
-          }
-          iconRight2To={"/new-chat"}
-          header="Messages"
+        }
+        iconRight2To={() => navigate('/new-chat')}
         />
       </nav>
 
