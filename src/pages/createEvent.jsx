57321f0b1416639650db739b47cf57ac3ec8658f
Brownie Tran <tknguyen2111@gmail.com>
2025-05-22 13:25:38 -0700

    Fix bugs on the navbar

--- src/pages/createEvent.jsx
+++ src/pages/createEvent.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import Navbar from "../components/layout/navbar";
 import BackButton from "../components/ui/backButton";
+import ChatIcon from "../components/ui/chatIcon";
 
 export default function CreateEvent() {
   const { isAuthenticated, isLoading, loginWithRedirect, getAccessTokenSilently } = useAuth0();
@@ -88,7 +89,7 @@ export default function CreateEvent() {
       <Navbar
         iconLeft={<BackButton />}
         header="PlayPal"
-        iconRight2={
+        iconRight={
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
@@ -96,10 +97,12 @@ export default function CreateEvent() {
             width="24px"
             fill="#000000"
           >
-            <path d="M80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z" />
+            <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
           </svg>
         }
-        iconRight2To={"/messages"}
+        iconRightTo={() => navigate("/profile")}
+        iconRight2={<ChatIcon />}
+        iconRight2To={() => navigate("/messages")}
       />
       <div className="container mx-auto px-4 py-6">
         <h2 className="text-2xl font-bold mb-6">Create New Event</h2>
